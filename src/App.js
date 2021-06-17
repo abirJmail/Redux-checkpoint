340a73c230dee019c827d79909af1213a34b8942
abirJmail <jmailabir@gmail.com>
2021-06-18 00:19:33 +0100

    redux

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import counterReducer from './JS/Reducers/counterReducer'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import Counter from './Components/Counter'
 
+const store = createStore(counterReducer,window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+
+ 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+   
+return (
+<Provider store={store}>
+<Counter />
+</Provider>
+);
 }
-
 export default App;
+
